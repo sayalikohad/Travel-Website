2e0b34695e9e332a305ba0e3bab74a354260b404
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2024-09-19 17:46:53 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const bars = document.querySelector("bar"),
-close = document.querySelector("close"),
-menu = document.querySelector("menu");
+const bars = document.querySelector(".bar"),
+close = document.querySelector(".close"),
+menu = document.querySelector(".menu");
 
+bars.addEventListener("click",() => {
+    menu.classList.add("active");
+    gsap.from(".menu" , {
+        opacity: 0,
+        duration:.3
+    })
+})
+
+close.addEventListener("click",() => {
+    menu.classList.add("active");
+})
