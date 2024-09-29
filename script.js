ddefb83efa122d1c3b6b56ec226efc4020b9183d
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2024-09-29 23:53:32 +0530

    script.js

--- script.js
+++ script.js
@@ -56,4 +56,4 @@ function scrollTirggerAnimation(triggerSelector, boxSelectors) {
 
 animateContent([".home .content h5, .home .content h1, .home .content p, .home .content .search "]);
 
-scrollTirggerAnimation(".travel", [".travel"])
+scrollTirggerAnimation(".travel", [".travel .box1", ".travel .box1", ".travel .box1"])
