7fbb2704d21560daeab51aed979e849dcc99de16
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2024-10-01 17:14:44 +0530

    script.js

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ const bars = document.querySelector(".bar"),
 close = document.querySelector(".close"),
 menu = document.querySelector(".menu");
 
-bars.addEventListener("click",() => {
+bars.addEventListener("click", () => {
     menu.classList.add("active");
-    gsap.from(".menu" , {
+    gsap.from(".menu", {
         opacity: 0,
-        duration:.3
+        duration: .3
     })
-    gsap.from(".menu ul" , {
+
+    gsap.from(".menu ul", {
         opacity: 0,
         x: -300
     })
 });
 
-close.addEventListener("click",() => {
+close.addEventListener("click", () => {
     menu.classList.remove("active")
 });
 
 function animateContent(selector) {
     selector.forEach((selector) => {
-        gsap.to(selector , {
-            y: 30, 
+        gsap.to(selector, {
+            y: 30,
             duration: 0.1,
             opacity: 1,
             delay: 0.2,
@@ -33,7 +34,7 @@ function animateContent(selector) {
 
 function scrollTirggerAnimation(triggerSelector, boxSelectors) {
     const timeline = gsap.timeline({
-        scrollTrigger : {
+        scrollTrigger: {
             trigger: triggerSelector,
             start: "top 50%",
             end: "top 80%",
@@ -41,8 +42,8 @@ function scrollTirggerAnimation(triggerSelector, boxSelectors) {
         },
     });
 
-    boxSelectors.forEach((boxSelectors) => {
-        timeline.to(boxSelectors, {
+    boxSelectors.forEach((boxSelector) => {
+        timeline.to(boxSelector, {
             y: 0,
             duration: 1,
             opacity: 1,
@@ -59,11 +60,12 @@ function swipeAnimation(triggerSelector, boxSelectors) {
             scrub: 3,
         },
     });
+
     boxSelectors.forEach((boxSelector) => {
         timeline.to(boxSelector, {
             x: 0,
             duration: 1,
-            opacity: 1,
+            opacity:1,
         });
     });
 }
@@ -90,8 +92,7 @@ function galleryAnimation(triggerSelector, boxSelectors) {
 
 
 
-
-animateContent([".home .content h5, .home .content h1, .home .content p, .home .content .search "]);
+animateContent([".home .content h5, .home .content h1, .home .content p, .home .content .search"]);
 
 scrollTirggerAnimation(".travel", [".travel .box1", ".travel .box2", ".travel .box3"]);
 
@@ -99,12 +100,13 @@ scrollTirggerAnimation(".feedback .container", [".feedback .label", ".feedback .
 
 scrollTirggerAnimation(".article", [".article .label", ".article .heading"]);
 
-swipeAnimation(".destinatons", [".destinatons .heading", ".destinatons .content"])
+swipeAnimation(".destinations", [".destinations .heading", ".destinations .content"])
 
 swipeAnimation(".article", [".article .latest-article", ".article .box1", ".article .box2", ".article .box3", ".article .box4"])
 
-galleryAnimation(".destinatons .gallery", [".destinatons .gallery .box1", ".destinatons .gallery .box2", ".destinatons .gallery .box3", ".destinatons .gallery .box4", ".destinatons .gallery .box5"])
+galleryAnimation(".destinations .gallery", [".destinations .gallery .box1",".destinations .gallery .box2",".destinations .gallery .box3",".destinations .gallery .box4",".destinations .gallery .box5"])
 
-galleryAnimation(".featured .gallery", [".featured .gallery .box1", ".featured .gallery .box2", ".featured .gallery .box3", ".featured.gallery .box4"])
+galleryAnimation(".featured .gallery", [".featured .gallery .box1",".featured .gallery .box2",".featured .gallery .box3",".featured .gallery .box4"])
 
-galleryAnimation(".feedback .voices", [".feedback .voices .box1", ".feedback .voices .box2", ".feedback .voices .box3", ".feedback .voices .box4", ".feedback .voices .box5", ".feedback .voices .box6"])
+galleryAnimation(".feedback .voices", [".feedback .voices .box1",".feedback .voices .box2",".feedback .voices .box3",".feedback .voices .box4",".feedback .voices .box5",".feedback .voices .box6"])
+
